e1a7d974d74a35eb0ecdf5ea75130e020a22f2f7
c2c-hackathon <hackathon.c2c@gmail.com>
2026-09-26 15:28:11 -0500

    increase tick speed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ connect_four.board.update_display()
 while True:
     try:
         connect_four.board.sync()
-        time.sleep(0.1)
+        time.sleep(0.05)
     except KeyboardInterrupt:
         # clear board
         print("\nClosing game...")
